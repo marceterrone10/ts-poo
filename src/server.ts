23406cc0dd01a8eb95cb677061607d0d3f43804e
marceterrone10 <marcelo.terrone10@gmail.com>
2025-08-10 17:18:34 -0300

    setup dependencias y inicio del servidor

---
+++ src/server.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+
+class ServerBootstrap {
+    public app: express.Application = express(); // Creamos una instancia de express
+    private port: number = 8000;
+
+    constructor(){
+        // Configuramos middlewares
+        this.app.use(express.json());
+        this.app.use(express.urlencoded({ extended: true }));
+        this.app.use(morgan('dev'));
+        this.app.use(cors());
+        this.listen();
+    }
+
+    public listen() { // para llamar a variables dentro de una clase usamos this
+        this.app.listen(this.port, () => {
+            console.log(`Server is running on port ${this.port}`);
+        });
+    };
+}
+
+new ServerBootstrap();
